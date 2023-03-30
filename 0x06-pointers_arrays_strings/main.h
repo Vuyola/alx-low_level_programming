52cde5b8a556c6526ba77c45def993d60453fc91
Vuyola <vuyompongose@gmail.com>
2023-03-30 13:16:02 -0700

    str cat

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,10 +2,10 @@
 #define MAIN_H
 
 int _putchar(char c);
-char *_strcat(char *dest, char *crs);
-char *_strcat(char *dest, char *crs, int n);
-char *_strncyp(char *dest, char *crs, int n);
-char *_strcmp(char *s1, char *s2);
+char *_strcat(char *dest, char *src);
+char *_strcat(char *dest, char *src, int n);
+char *_strncyp(char *dest, char *src, int n);
+char _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *a);
 char *cap_string(char *a);
