d056075a342f456ddf4252f685946b10328ee221
Vuyola <vuyompongose@gmail.com>
2023-03-31 16:21:14 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - adds two value
+ *@n1: the first number to be comuted
+ *@n2: the second number to be comuted
+ * Return: integer value
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
