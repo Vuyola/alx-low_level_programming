9158386ba09f5e1bb4ca6e2f90aabcc65b550e10
Vuyola <vuyompongose@gmail.com>
2023-04-04 12:02:40 -0700

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcpyl - copy a string
+ * @dest: destination value
+ * @src: source value
+ * Return: the pointer to dest
+ */
+char *_strcpyl(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i++] = '\0';
+	return (dest);
+}
